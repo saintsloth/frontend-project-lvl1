f5c4b3b72f6a850b7c545d5af940675e807e9b81
saintsloth <dakaprielov@gmail.com>
2022-01-10 17:03:57 +0300

    step 8 complete

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import cli from '../cli.js';
+import { getRandomInt, miniGame } from '../index.js';
+import greeting from '../brain-games.js';
+
+const game = (name) => {
+  console.log('What number is missing in the progression?');
+
+  for (let i = 0; i < 3; i += 1) {
+    const arrayLength = getRandomInt(5, 10);
+    let arrayFirstNum = 1;
+    const array = [arrayFirstNum];
+    const arrayShift = getRandomInt(1, 10);
+    for (let ii = 1; ii < arrayLength; ii += 1) {
+      arrayFirstNum += arrayShift;
+      array.push(arrayFirstNum);
+    }
+    const numOfHiddenNum = getRandomInt(2, arrayLength);
+    const question = `${array.slice(0, numOfHiddenNum - 1)} .. ${array.slice(numOfHiddenNum)}`;
+    const trueAnswer = `${array[numOfHiddenNum - 1]}`;
+    if (miniGame(name, question, trueAnswer) === false) return;
+  }
+  console.log(`Congratulations, ${name}`);
+};
+
+greeting();
+const name = cli();
+game(name);
