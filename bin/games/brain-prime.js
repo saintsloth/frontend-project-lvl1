01b5f47c926d54a7cffaafdd75af7b43ec96b00b
saintsloth <dakaprielov@gmail.com>
2022-01-10 17:14:38 +0300

    step 9 complete

---
+++ bin/games/brain-prime.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import cli from '../cli.js';
+import { getRandomInt, miniGame } from '../index.js';
+import greeting from '../brain-games.js';
+
+const game = (name) => {
+  function getTrueAnswer(num) {
+    let flag = true;
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        flag = false;
+        break;
+      }
+    }
+    return flag ? 'yes' : 'no';
+  }
+
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const num = getRandomInt();
+    const question = `${num}`;
+    const trueAnswer = `${getTrueAnswer(num)}`;
+    if (miniGame(name, question, trueAnswer) === false) return;
+  }
+  console.log(`Congratulations, ${name}`);
+};
+
+greeting();
+const name = cli();
+game(name);
